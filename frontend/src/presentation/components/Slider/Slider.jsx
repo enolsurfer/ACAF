f00e2439d152325564315eace149008ae4ed5de9
enolsurfer <39172327+enolsurfer@users.noreply.github.com>
2024-04-26 14:24:22 +0200

    Slider test

--- frontend/src/presentation/components/Slider/Slider.jsx
+++ frontend/src/presentation/components/Slider/Slider.jsx
@@ -34,7 +34,7 @@ export default function Slider() {
         </SwiperSlide>
       </Swiper>
       <h1 className="sliderTitle">
-        ASOCIACIÓN <br /> CULTURAL ARENESCA <br /> DE FESTEJOS
+        CIACIÓN <br /> CULTURAL ARENESCA <br /> DE FESTEJOS
       </h1>
     </div>
   );
